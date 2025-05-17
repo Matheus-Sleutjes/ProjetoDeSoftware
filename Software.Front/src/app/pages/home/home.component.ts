379377f605fc15f81c6609fac1c202954cd243f9
Wagner torres <wvinicius.torres@gmail.com>
2025-05-16 21:13:11 -0400

    front end home

--- Software.Front/src/app/pages/home/home.component.ts
+++ Software.Front/src/app/pages/home/home.component.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components_utils/header/header.component";
+import { Router } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
+
+interface Option {
+  icon: string;
+  label: string;
+}
+
+interface Clinic {
+  name: string;
+  mapUrl: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, SharedModule, HeaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
-  standalone: true,
+  styleUrls: ['./home.component.scss'],
+  imports: [SharedModule],
+  standalone: true
 })
 export class HomeComponent {
+  options: Option[] = [
+    { icon: 'favorite_outline', label: 'Cardiologista', },
+    { icon: 'vaccines', label: 'Vacinas' },
+    { icon: 'favorite_outline', label: 'Cardiologista' },
+    { icon: 'favorite_outline', label: 'Cardiologista' },
+    { icon: 'favorite_outline', label: 'Cardiologista' },
+    { icon: 'favorite_outline', label: 'Cardiologista' },
+  ];
 
+  clinics: Clinic[] = [
+    { name: 'Cuiabá – MT', mapUrl: 'assets/imgs/mapa-cuiaba.png' },
+    { name: 'São Paulo – SP', mapUrl: 'assets/imgs/mapa-sp.png' }
+  ];
+
+  constructor(private router: Router) { }
+
+  logout() {
+    // seu código de logout aqui...
+    this.router.navigate(['/login']);
+  }
 }
