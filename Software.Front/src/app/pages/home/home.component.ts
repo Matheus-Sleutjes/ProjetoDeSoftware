a483defdc855e2dc160d7897ba62c807bffc27d5
Matheus-Sleutjes <sleutjes13@gmail.com>
2025-10-26 11:41:14 -0400

    fix: menu inicial tela home ajustado

--- Software.Front/src/app/pages/home/home.component.ts
+++ Software.Front/src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UtilsService } from '../../services/utils.service';
 import { AuthenticationService } from '../../services/authentication.service';
+import { MenuItens } from '../../menu-item';
 
 interface Option {
   icon: string;
@@ -22,16 +23,11 @@ interface Clinic {
   standalone: true
 })
 export class HomeComponent {
-  options: Option[] = [
-    { icon: 'fa-user-md', label: 'Médicos', route: 'doctors' },
-    { icon: 'fa-user', label: 'Pacientes', route: 'patients' },
-    { icon: 'fa-calendar-alt', label: 'Agendamentos', route: 'appointments' },
-    { icon: 'fa-stethoscope', label: 'Especialidades', route: 'specialties' },
-  ];
+  options: any[] = MenuItens;
 
   clinics: Clinic[] = [
-    { name: 'Cuiabá – MT' },
-    { name: 'São Paulo – SP' }
+    { name: 'Cuiabá  MT' },
+    { name: 'São Paulo  SP' }
   ];
 
   name = '';
@@ -59,36 +55,6 @@ export class HomeComponent {
   }
 
   navigateToAppointments(pageOpt: Option) {
-    console.log('Navegando para:', pageOpt.route);
     this.router.navigate([`/${pageOpt.route}`]);
-  }
-
-  navigateToRegisterUser() {
-    this.router.navigate(['/register']);
-  }
-
-  navigateToListUser() {
-    this.router.navigate(['/users']);
-  }
-
-  navigateToHistory() {
-    // Por enquanto, mostrar alerta - implementar quando a página estiver pronta
-    alert('Funcionalidade de Histórico será implementada em breve!');
-  }
-
-  navigateToAppointmentsList() {
-    this.router.navigate(['/appointments']);
-  }
-
-  navigateToDoctors() {
-    this.router.navigate(['/doctors']);
-  }
-
-  navigateToPatients() {
-    this.router.navigate(['/patients']);
-  }
-
-  navigateToSpecialties() {
-    this.router.navigate(['/specialties']);
   }
 }
